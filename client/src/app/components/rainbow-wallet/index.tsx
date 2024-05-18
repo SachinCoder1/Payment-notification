6a62663f46fd6cf966c43b8c790fe572da1a9d0f
Sachin Gurjar <sachinhlo232@gmail.com>
2024-05-18 15:46:15 +0530

    wallet connect using rainbow

---
+++ client/src/app/components/rainbow-wallet/index.tsx
@@ -0,0 +1,12 @@
+"use client";
+
+import React from "react";
+import { ConnectButton } from "@rainbow-me/rainbowkit";
+
+export default function RainbowMyCustom() {
+  return (
+    <div>
+      <ConnectButton />
+    </div>
+  );
+}
