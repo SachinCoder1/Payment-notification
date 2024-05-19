78bf2b11c3b4f7a15956aa4efd69b2b3fe0842e4
recognizeyourself <recognizeyourself909@gmail.com>
2024-05-19 13:10:44 +0530

    fx tst

--- backend/src/routes/auth.ts
+++ backend/src/routes/auth.ts
@@ -1,6 +1,6 @@
 import express from "express";
-import test from "node:test";
-import { alchemyWebhooks, signupWithEmail } from "~/controller/auth";
+
+import { alchemyWebhooks, signupWithEmail, test } from "~/controller/auth";
 
 const router = express.Router();
 
